605fe9ce45755fa2e5ef23648028bb9cc646ed11
manarabdelaty <manarabdelatty@aucegypt.edu>
2021-01-23 16:37:41 +0200

    DV update

--- verilog/dv/caravel/user_proj_example/io_ports/io_ports_tb.v
+++ verilog/dv/caravel/user_proj_example/io_ports/io_ports_tb.v
@@ -17,17 +17,85 @@
 
 `timescale 1 ns / 1 ps
 
-`include "caravel.v"
+`define   TEST_FILE   "../sw/test.hex" 
+`define   SIM_TIME    1500_000
+`define   SIM_LEVEL   0
+
+`define SOC_SETUP_TIME 800*2001
+
+`define NO_HC_CACHE
+
 `include "spiflash.v"
+
+`include "sst26wf080b.v"
+`include "23LC512.v"
+`include "caravel.v"
+
+`ifdef SIM
+
+`ifdef FAST
+    `define USE_DFFRAM_BEH
+    `define NO_HC_CACHE
+    `include "DFFRAM_beh.v"
+`else
+	`ifndef GL_UA
+		`include "user_project/IPs/DFFRAM_4K.v"
+		`include "user_project/IPs/DFFRAMBB.v"
+		`include "user_project/IPs/DMC_32x16.v"
+	`endif
+`endif
+
+`ifdef GL_UA
+	`include "gl/user_project/gl/apb_sys_0.v"
+	`include "gl/user_project/gl/DFFRAM_4K.v"
+	`include "gl/user_project/gl/DMC_32x16.v"
+	`include "gl/user_project/gl/el2_swerv_wrapper.v"
+	`include "gl/user_project/gl/user_project_wrapper.v"
+`else
+
+`include "user_project/AHB_sys_0/AHBlite_sys_0.v"
+`include "user_project/AHB_sys_0/AHBlite_bus0.v"
+`include "user_project/AHB_sys_0/AHBlite_GPIO.v"
+`include "user_project/AHB_sys_0/AHBlite_db_reg.v"
+
+`include "user_project/AHB_sys_0/APB_sys_0/APB_WDT32.v"
+`include "user_project/AHB_sys_0/APB_sys_0/APB_TIMER32.v"
+`include "user_project/AHB_sys_0/APB_sys_0/APB_PWM32.v"
+`include "user_project/AHB_sys_0/APB_sys_0/AHB_2_APB.v"
+`include "user_project/AHB_sys_0/APB_sys_0/APB_bus0.v"
+`include "user_project/AHB_sys_0/APB_sys_0/APB_sys_0.v"
+
+`include "user_project/IPs/TIMER32.v"
+`include "user_project/IPs/PWM32.v"
+`include "user_project/IPs/WDT32.v"
+`include "user_project/IPs/spi_master.v"
+`include "user_project/IPs/i2c_master.v"
+`include "user_project/IPs/GPIO.v"
+`include "user_project/IPs/APB_UART.v"
+`include "user_project/IPs/APB_SPI.v"
+`include "user_project/IPs/APB_I2C.v"
+`include "user_project/IPs/AHBSRAM.v"
+`include "user_project/acc/AHB_SPM.v"
+
+`include "user_project/IPs/QSPI_XIP_CTRL.v"
+`include "user_project/IPs/DMC_32x16.v"
+`include "user_project/IPs/RAM_1024x32.v"
+
+`include "user_project/el2_n5_soc_wrapper.v"
+`include "user_project/el2.v"
+`include "user_project/soc_core.v"
+
+`endif
+`endif
 
 module io_ports_tb;
 	reg clock;
-    	reg RSTB;
+    reg RSTB;
 	reg power1, power2;
 	reg power3, power4;
 
-    	wire gpio;
-    	wire [37:0] mprj_io;
+	wire gpio;
+	wire [37:0] mprj_io;
 	wire [7:0] mprj_io_0;
 
 	assign mprj_io_0 = mprj_io[7:0];
@@ -42,44 +110,70 @@ module io_ports_tb;
 		clock = 0;
 	end
 
+	// GPIO Loopback!
+    // wire [13:0] GPIO_PINS;
+    // generate
+    //     genvar i;
+    //     for(i=0; i<14; i=i+1)
+    //         assign GPIO_PINS[i] = GPIOOEN_Sys0_S2[i] ? GPIOOUT_Sys0_S2[i] : 1'bz;
+    // endgenerate
+
+    // assign GPIO_PINS[15:8] = GPIO_PINS[7:0];
+    // assign GPIOIN_Sys0_S2 = GPIO_PINS;
+
+	// Serial Terminal connected to UART0 TX*/
+    terminal term(.rx(mprj_io[21]));  // RsTx_Sys0_SS0_S0
+
+    // SPI SRAM connected to SPI0
+    wire SPI_HOLD = 1'b1;
+    M23LC512 SPI_SRAM(
+        .RESET(~RSTB),
+        .SO_SIO1(mprj_io[24]),  // MSI_Sys0_SS0_S2
+        .SI_SIO0(mprj_io[25]),  // MSO_Sys0_SS0_S2
+        .CS_N(mprj_io[26]),     // SSn_Sys0_SS0_S2
+        .SCK(mprj_io[27]),      // SCLK_Sys0_SS0_S2
+        .HOLD_N_SIO3(SPI_HOLD)
+	);
+
+	// I2C E2PROM connected to I2C0
+    wire    scl, sda;
+    delay   m0_scl (scl_oen_o_Sys0_SS0_S4 ? 1'bz : scl_o_Sys0_SS0_S4, scl),
+            m0_sda (sda_oen_o_Sys0_SS0_S4 ? 1'bz : sda_o_Sys0_SS0_S4, sda);
+
+    assign  scl_i_Sys0_SS0_S4 = scl;
+    assign  sda_i_Sys0_SS0_S4 = sda;
+    
+	pullup p1(scl); // pullup scl line
+	pullup p2(sda); // pullup sda line
+
+    M24LC16B I2C_E2PROM(
+        .A0(1'b0), 
+        .A1(1'b0), 
+        .A2(1'b0), 
+        .WP(1'b0), 
+        .SDA(sda), 
+        .SCL(scl), 
+        .RESET(~HRESETn)
+    );
+
+	initial begin
+		// Load the application into the N5 flash memory
+		#1  $readmemh(`TEST_FILE, flash.I0.memory);
+		$display("---------N5 Flash -----------");
+		$display("Memory[0]: %0d, Memory[1]: %0d, Memory[2]: %0d, Memory[3]: %0d", 
+            flash.I0.memory[0], flash.I0.memory[1], flash.I0.memory[2], flash.I0.memory[3]);
+	end
+
 	initial begin
 		$dumpfile("io_ports.vcd");
 		$dumpvars(0, io_ports_tb);
 
-		// Repeat cycles of 1000 clock edges as needed to complete testbench
-		repeat (25) begin
-			repeat (1000) @(posedge clock);
-			// $display("+1000 cycles");
-		end
-		$display("%c[1;31m",27);
-		$display ("Monitor: Timeout, Test Mega-Project IO Ports (RTL) Failed");
-		$display("%c[0m",27);
-		$finish;
-	end
-
-	initial begin
-	    // Observe Output pins [7:0]
-	    wait(mprj_io_0 == 8'h01);
-	    wait(mprj_io_0 == 8'h02);
-	    wait(mprj_io_0 == 8'h03);
-    	    wait(mprj_io_0 == 8'h04);
-	    wait(mprj_io_0 == 8'h05);
-            wait(mprj_io_0 == 8'h06);
-	    wait(mprj_io_0 == 8'h07);
-            wait(mprj_io_0 == 8'h08);
-	    wait(mprj_io_0 == 8'h09);
-            wait(mprj_io_0 == 8'h0A);   
-	    wait(mprj_io_0 == 8'hFF);
-	    wait(mprj_io_0 == 8'h00);
-
-	    $display("Monitor: Test 1 Mega-Project IO (RTL) Passed");
-	    $finish;
-	end
-
-	initial begin
 		RSTB <= 1'b0;
 		#2000;
 		RSTB <= 1'b1;	    // Release reset
+		#(`SOC_SETUP_TIME);
+		#(`SIM_TIME);
+	    $finish;
 	end
 
 	initial begin		// Power-up sequence
@@ -95,10 +189,6 @@ module io_ports_tb;
 		power3 <= 1'b1;
 		#200;
 		power4 <= 1'b1;
-	end
-
-	always @(mprj_io) begin
-		#1 $display("MPRJ-IO state = %b ", mprj_io[7:0]);
 	end
 
 	wire flash_csb;
@@ -129,7 +219,7 @@ module io_ports_tb;
 		.vssd2	  (VSS),
 		.clock	  (clock),
 		.gpio     (gpio),
-        	.mprj_io  (mprj_io),
+        .mprj_io  (mprj_io),
 		.flash_csb(flash_csb),
 		.flash_clk(flash_clk),
 		.flash_io0(flash_io0),
@@ -147,6 +237,34 @@ module io_ports_tb;
 		.io2(),			// not used
 		.io3()			// not used
 	);
+
+	/* N5 Flash */
+    sst26wf080b flash(
+        .SCK(mprj_io[18]),     // fsclk
+        .SIO(mprj_io[17:14]),  // fdo
+        .CEb(mprj_io[19])      // fcen
+    );
+
+endmodule
+
+module terminal #(parameter bit_time = 400) (input rx);
+
+    integer i;
+    reg [7:0] char;
+    initial begin
+        forever begin
+            @(negedge rx);
+            i = 0;
+            char = 0;
+            #(3*bit_time/2);
+            for(i=0; i<8; i=i+1) begin
+                char[i] = rx;
+                #bit_time;
+            end
+            $write("%c", char);
+        end
+    end
+
 
 endmodule
 `default_nettype wire
---
+++ verilog/dv/caravel/user_proj_example/sw/test.c
@@ -0,0 +1,138 @@
+#include "n5_drv.h"
+#include "n5_int.h"
+
+unsigned int A[100];
+
+void IRQ() {
+    gpio_write(0x0099);        
+}
+
+int fact(int n){
+    int f = 1;
+    for(int i=2; i<=n; i++)
+        f = f * i;
+    return f;
+}
+
+int strlen(char *s){
+    int i=0;
+    while (*s){
+        i++;
+    }
+    return i;
+}
+
+void M23LC_write_byte(int n, unsigned int addr, unsigned int data){
+  spi_start(n);
+  spi_write(n, 0x2);
+  spi_write(n, addr >> 8);     // Address high byte
+  spi_write(n, addr & 0xFF);   // Address low byte
+  spi_write(n, data);
+  spi_end(n);
+}
+
+unsigned char M23LC_read_byte(int n, unsigned short addr){
+  spi_start(n);
+  spi_write(n, 0x3);
+  spi_write(n, addr >> 8);     // Address high byte
+  spi_write(n, addr & 0xFF);   // Address low byte
+  spi_write(n, 0);             // just write a dummy data to get the data out
+  spi_end(n);
+  return spi_read(n);
+}
+
+#define     DELAY(n)   for(int i=0; i<n; i++)
+
+int main(){
+    // Initialization
+    uart_init (0, 0);
+    gpio_set_dir(0x00FF);
+
+    spi_init(0, 0,0,40);
+    
+    // Start the test
+    uart_puts (0, "Hello World!\n", 13);
+    
+    // I2C testing
+    uart_puts (0, "I2C Test: ", 10);
+    i2c_init(0, 16000);
+    i2c_start(0, 0xA0);    // slave address = 1010_000, R/W_b=0 (write)
+    i2c_sendByte(0x0);
+    i2c_sendByte(0x0);
+    i2c_sendByte(0x55);
+    i2c_stop();
+    DELAY(20);
+    i2c_start(0, 0xA0);    // slave address = 1010_000, R/W_b=0 (write)
+    i2c_sendByte(0x0);
+    i2c_sendByte(0x0);
+    i2c_start(0, 0xA1);    // slave address = 1010_000, R/W_b=1 (read)
+    int i2c_data = i2c_readByte();
+    gpio_write(i2c_data);
+    if(i2c_data == 0x55)
+        uart_puts(0,"Passed!\n", 8);
+    else
+        uart_puts(0,"Failed!\n", 8);
+    i2c_stop();
+
+    // GPIO
+    uart_puts (0, "GPIO Test: ", 11);
+
+    int gpio_val = 0x55;
+    if (SIM_SOC == 0){
+        gpio_val = 0x15; // Wrapper has a lower number of GPIOs
+    }
+    
+    gpio_write(gpio_val);
+    DELAY(50);
+    int gpio_data = gpio_read();
+    if((gpio_data >> 8) == gpio_val)
+        uart_puts(0,"Passed!\n", 8);
+    else
+        uart_puts(0,"Failed!\n", 8);
+    
+    // SPI
+    uart_puts (0, "SPI Test: ", 10);
+    M23LC_write_byte(0, 0, 0xA5);
+    unsigned int spi_data = M23LC_read_byte(0, 0);
+    DELAY(10);
+    if(spi_data==0xA5)
+        uart_puts(0,"Passed!\n", 8);
+    else 
+        uart_puts(0,"Failed!\n", 8);
+
+    // Timer
+    uart_puts (0, "TMR Test: ", 10);
+    tmr_init(0, 800, 10);
+    tmr_enable(0);
+    tmr_wait(0);
+    tmr_disable(0);
+    if(tmr_read(0) == 0)
+        uart_puts(0,"Passed!\n", 8);
+    else 
+        uart_puts(0,"Failed!\n", 8);
+
+    // WDT
+    uart_puts (0, "WDT Test: ", 10);
+    wdt_init(0);
+    wdt_enable(0);
+    wdt_load(0, 50);
+    while(wdt_read(0) != 0);
+    wdt_disable(0);
+    if (wdt_read(0) == 0)
+        uart_puts(0,"Passed!\n", 8);
+    else
+        uart_puts(0,"Failed!\n", 8);
+
+    // PWM
+    pwm_init(0, 250, 99, 5);
+ 	pwm_enable(0);
+    DELAY(30);
+    pwm_disable(0); 
+
+    // Some Delay
+    DELAY(50);
+   
+    // Done!
+    uart_puts(0, "Done!\n\n", 7);
+    return 0;
+}
